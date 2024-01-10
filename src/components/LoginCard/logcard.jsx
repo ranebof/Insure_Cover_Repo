47351ce61a4860b0984cc97aefb5c5fdb0f7a04a
Pasha <kramarpavlo@gmail.com.com>
2024-01-10 20:21:32 +0200

    updated login card

--- src/components/LoginCard/logcard.jsx
+++ src/components/LoginCard/logcard.jsx
@@ -5,15 +5,23 @@ export default function LogCard() {
     return (
         <div className="main_container">
             <div className="card_container_left">
-               <div className="text_container_l"> 
-                <span className="make_acc_text">Створити Обліковий запис</span>
-               </div>
-                <div className="circle_l"><img src="./images/google_ico.png" className="google_ico"></img></div>
+                <div className="text_container_l">
+                    <span className="make_acc_text">Створити Обліковий запис</span>
+                </div>
+                <div className="circle_l"><img src="./images/google_ico.png" className="google_ico" alt=""></img></div>
                 <div className="text_container_l_second"></div>
-                    <span className="use_email">Або використайте вашу пошту для реєстрації </span>
+                <span className="use_email">Або використайте вашу пошту для реєстрації </span>
+                <form className="log-form">
+                    <input className="form-field" type="text" placeholder="Ім`я" />
+                    <input className="form-field" type="text" placeholder="Прізвище" />
+                    <input className="form-field" type="email" placeholder="Email" />
+                </form>
+                <div className="log-button">
+                    <button className="l-button">Далі</button>
+                </div>
             </div>
             <div className="card_container_right">
-                <img className="image_r" src="./images/login_background.png"></img>
+                <img className="image_r" src="./images/login_background.png" alt=""></img>
             </div>
         </div>
     )
