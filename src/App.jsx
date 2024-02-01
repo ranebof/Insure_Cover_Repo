1f82069e4b38e98379a05f0d177d002e213e1e7c
ranebof <117946796+ranebof@users.noreply.github.com>
2024-02-01 22:11:30 +0200

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/MainHeader/header';
 import Footer from './components/MainFooter/footer';
-//import LogCard from './components/LoginCard/logcard';
+import LogCard from './components/LoginCard/logcard';
 import Table from './components/CompaniesTable/table';
 // import RegCard from './components/RegisterCard/regcard';
 
@@ -10,8 +10,8 @@ export default function App() {
   return (
     <div className="Main">
       <Header />
-      {/* <LogCard /> */}
-      <Table />
+      <LogCard />
+      {/* <Table /> */}
       <Footer />
     </div>
   )
