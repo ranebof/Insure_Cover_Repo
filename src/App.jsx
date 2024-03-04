c8f0bd86b8307bdd5d6eb019268f3b15a9a6ad72
ranebof <117946796+ranebof@users.noreply.github.com>
2024-03-04 20:35:00 +0200

    Added React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import Footer from './components/MainFooter/footer';
 import LogCard from './components/LoginCard/logcard';
 import Table from './components/CompaniesTable/table';
 // import RegCard from './components/RegisterCard/regcard';
+import { BrowserRouter, Routes, Router, Route } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <div className="Main">
-      <Header />
-      <LogCard />
-      {/* <Table />  */}
-      <Footer /> 
+      <BrowserRouter>
+        <Routes>
+        {/* <Header />
+          
+          <Table />
+          <Footer /> */}
+        <Route path='/logincard' element={<LogCard />} />
+          
+        </Routes> 
+      </BrowserRouter>
     </div>
   )
 }
