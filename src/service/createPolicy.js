cd2731b37c733cdc7a2ef5b920bdf5c96505bd13
ranebof <117946796+ranebof@users.noreply.github.com>
2024-03-29 03:59:38 +0200

    created a service

--- src/service/createPolicy.js
+++ src/service/createPolicy.js
@@ -2,3 +2,19 @@ import axios from "axios";
 
 const API = "http://16.170.143.105:8000/api/";
 
+const createPolicy = (number,name,description,company) => {
+    return axios.post(API + "create/",{
+        number,name,description,company
+    },{
+        headers: {
+            "Accept":"*/*",
+            "Content-Type": "application/json",
+        }
+    })
+}
+
+const createService =  {
+    createPolicy
+}
+
+export default createService
--- src/service/dist/createPolicy.dev.js
+++ src/service/dist/createPolicy.dev.js
@@ -1,7 +1,32 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
 var _axios = _interopRequireDefault(require("axios"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var API = "http://16.170.143.105:8000/api/";
+
+var createPolicy = function createPolicy(number, name, description, company) {
+  return _axios["default"].post(API + "create/", {
+    number: number,
+    name: name,
+    description: description,
+    company: company
+  }, {
+    headers: {
+      "Accept": "*/*",
+      "Content-Type": "application/json"
+    }
+  });
+};
+
+var createService = {
+  createPolicy: createPolicy
+};
+var _default = createService;
+exports["default"] = _default;
