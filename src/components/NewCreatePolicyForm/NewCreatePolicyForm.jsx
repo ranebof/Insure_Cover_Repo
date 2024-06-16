428b9027b8d15af13f580b90a8639b52885d5f67
Pasha <kramarpavlo@gmail.com.com>
2024-06-16 19:50:31 +0300

    fix

--- src/components/NewCreatePolicyForm/NewCreatePolicyForm.jsx
+++ src/components/NewCreatePolicyForm/NewCreatePolicyForm.jsx
@@ -9,7 +9,14 @@ export default function NewCreatePolicyForm() {
   const [company, setCompany] = useState("");
   const [diseases, setDiseases] = useState([]);
   const [medicines, setMedicines] = useState([]);
-  const [description, setDescriptions] = useState("");
+  const [description, setDescription] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowPopup(true);
+  };
+
   const handleBtnClick = () => {
     if (!policyCode.trim() || !policyName.trim() || !company.trim()) {
       toast.error("Будь ласка, заповніть всі поля");
@@ -49,15 +56,15 @@ export default function NewCreatePolicyForm() {
         onChange={(e) => setCompany(e.target.value)}
       />
       <form className="form" onSubmit={handleSubmit}>
-          <div className="last-form-group-big">
-            <textarea
-              className="desc-input"
-              type="text"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </div>
-        </form>
+        <div className="last-form-group-big">
+          <textarea
+            className="desc-input"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+      </form>
       <button className="button" onClick={handleBtnClick}>
         ДАЛІ
       </button>
